b769ecd1365bbc5048af72c2aed2729a8990d268
kawan <kawan2018@yahoo.com>
2021-01-19 14:07:18 +0300

    sec

---
+++ resources/views/layout/nav.blade.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=divice-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edg">
+<title>Pharma</title>
+<link real="stylesheet" href="{{asset('/assets/css/style.css')}}">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+<body class="bg-gradient-success">
+    @yield('content')
+
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+
+Auth::routes();
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
