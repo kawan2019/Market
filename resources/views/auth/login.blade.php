4def452150caeb7cd529af1f149e419c9832f92b
kawan <kawan2018@yahoo.com>
2021-02-04 18:17:41 +0300

    bro

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,7 +14,7 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control rounded-0 @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="email" placeholder="Email" type="email" class="form-control rounded-0 @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -28,7 +28,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control rounded-0  @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input id="password" placeholder="Password" type="password" class="form-control rounded-0  @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -39,17 +39,32 @@
                         </div>
 
                         <div class="form-group row align-content-center justify-content-center">
-                            <div class="col-md-9 ">
+                            <div class="col-md-5 ">
                                 <button type="submit" style="background-color:#0165b6;" class=" text-white btn rounded-0 mt-3 w-100 form-control">
                                     {{ __('Login') }}
                                 </button>
-                            
+                            </div>
+                            <div class="col-md-5 ">
+                                <button type="" id="cllde" class=" btn-outline-aure  btn rounded-0 mt-3 w-100">
+                                    Try Demo
+                                </button>
                             </div>
                         </div>
                     </form>
+                    
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+  $("#cllde").click(function(){
+    $("#email").val("guest@unknown.com");
+    $("#password").val("12345678");
+  });
+});
+</script>
 @endsection
--- resources/views/stocks.blade.php
+++ resources/views/stocks.blade.php
@@ -18,15 +18,15 @@
     <form action="/buy/0/0" method="POST" class=" text-center">
         @csrf
         <div class="row mt-3 justify-content-center">
-            <div class="col text-center col-lg-4 col-12 mt-3">
+            <div class="col text-center col-lg-4 col-12 mt-2">
                 <label class=" text-white">Barcode Stocks</label>
                 <input name="id" type="text"placeholder="Barcode Stocks"class=" form-control   rounded-0 border-0">
             </div>
-            <div class="col text-center col-lg-4 col-12 mt-3">
+            <div class="col text-center col-lg-4 col-12 mt-2">
                 <label class=" text-white">Name Stocks</label>
                 <input name="name" type="text"placeholder="Name Stocks"class=" form-control   rounded-0 border-0">
             </div>
-            <div class="col text-center col-lg-4 col-12 mt-3">
+            <div class="col text-center col-lg-4 col-12 mt-2">
                 <label class=" text-white">Supplier</label>
             <select name="supplier_id" id="" class="form-control rounded-0 border-0" required>
                 @foreach ($suppliers as $supplier)
@@ -34,26 +34,26 @@
                 @endforeach
             </select>
             </div>
-            <div class="col text-center col-lg-4 col-12 mt-3">
+            <div class="col text-center col-lg-4 col-12 mt-2">
                 <label class=" text-white">Count</label>
                 <input name="count" type="number"placeholder="Count"class=" form-control   rounded-0 border-0">
             </div>
-            <div class="col text-center col-lg-4 col-12 mt-3">
+            <div class="col text-center col-lg-4 col-12 mt-2">
                 <label class=" text-white">Price</label>
                 <input name="price" type="number"placeholder="Price"class=" form-control   rounded-0 border-0">
             </div>
-            <div class="col text-center col-lg-4 col-12 mt-3">
+            <div class="col text-center col-lg-4 col-12 mt-2">
                 <label class="text-white">Expire Date</label>
                 <input name="expire_date" type="date"class="form-control rounded-0 border-0">
             </div>
-            <div class="col text-center col-lg-4 col-12 mt-3">
+            <div class="col text-center col-lg-4 col-12 mt-2">
                 <label class=" text-white">Is debt</label>
             <select name="is_debt" id="" class="form-control rounded-0 border-0" required>
                 <option value="0">No</option>
                 <option value="1">Yes</option>
             </select>
             </div>
-            <div class="col text-center col-lg-4 col-12 mt-3">
+            <div class="col text-center col-lg-4 col-12 mt-2">
                 <label class=" text-white">Type</label>
             <select name="type" id="" class="form-control rounded-0 border-0" required>
                 <option value="0">Barcode</option>
@@ -62,7 +62,7 @@
             </div>
             
         </div>
-        <button style="background-color:#025091;"class=" text-white btn rounded-0 mt-5 border-white">Buy New Item</button>
+        <button style="background-color:#025091;"class=" text-white btn rounded-0 mt-4 border-white">Buy New Item</button>
     </form>
     <hr>
     @include('layout.card')
